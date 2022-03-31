ebaa4c4ee07304edd731d87c1ca63bcd3ec26ded
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-03-31 16:18:43 +0300

    Complete task2

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,20 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(N) {
+  if (N < 0) {
+    return null;
+  }
+
+  const result = [];
+  const abc = 'abcdefghijklmnopqrstuvwxyz';
+
+  for (let i = 0; i < N; i++) {
+    const row = [];
+    for (let j = i; j < N + i; j++) {
+      row.push(abc[j % 26]);
+    }
+    result.push(row.join(' '));
+  }
+
+  return result.join('\n');
 }
+
+console.log(calculate(5));
