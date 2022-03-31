6d53e68348406cf98579929a7371d5126cfb677f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-03-31 15:39:19 +0300

    Complete task1

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,17 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(str) {
+  const [leftDots, sign, rightDots] = str.split(' ');
+
+  const num1 = leftDots.length;
+  const num2 = rightDots.length;
+
+  const operations = {
+    '+': num1 + num2,
+    '-': num1 - num2,
+    '*': num1 * num2,
+    '//': num1 / num2,
+  };
+
+  return '.'.repeat(operations[sign]);
 }
+
+console.log(calculate('...... // ..'));
