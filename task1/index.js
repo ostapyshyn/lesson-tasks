722987c6403dd9945084191b1ba2ab8122499eda
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-04-03 16:12:01 +0300

    Add type check

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,9 @@
 function calculate(str) {
+  if (typeof str !== 'string' || str === '') {
+    console.log('Variable is not a string or empty');
+    return '';
+  }
+
   const [leftDots, sign, rightDots] = str.split(' ');
 
   const num1 = leftDots.length;
