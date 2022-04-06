82bdfbe9ccd0be00d74e57c47b5cf33dcac271de
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-04-06 19:00:38 +0300

    Add another solution for js-9-task

--- lesson2/js-9.js
+++ lesson2/js-9.js
@@ -1,3 +1,14 @@
+// function filter(arr, fn) {
+//   return arr.filter(fn);
+// }
+
+// alternative
 function filter(arr, fn) {
-  return arr.filter(fn);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
